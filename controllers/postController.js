1329c7eac458fa7ed6f12e99c8547cd78b3045f4
JONAS KALLWIES <s0578216@htw-berlin.de>
2023-05-04 17:30:23 +0200

    small changes on postController and added post.ejs

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,6 @@
 exports.postPage = (req, res) => {
     let postId = req.params.postId;
-    res.send(`This is the page for the post of id: ${postId}`);
+    res.render("post.ejs", {postId: postId});
 };
 exports.createPostPage = (req, res) => {
     res.send(`This is the page to create a new post!`);
