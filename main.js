2d1d245589beb3c1929a7030136ee8496a72432c
SOENKE SCHAARSCHMIDT <s0577919@htw-berlin.de>
2023-05-04 17:41:56 +0200

    Sprint_03

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const router = require('./router')
 const layouts = require("express-ejs-layouts")
 const port = 3000, express = require("express"), app = express();
+const path = require("path")
 
 app.set("view engine", "ejs")
 app.use(layouts)
 
 app.use(express.json());
 app.use('/', router);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
  console.log(`The Express.js server has started and is listening on port number: ${port}`);
