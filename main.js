49695705ee0a716be772ac003868803f86f19c88
SOENKE SCHAARSCHMIDT <s0577919@htw-berlin.de>
2023-05-04 18:12:57 +0200

    Sprint_03 - Add ErrorHandling

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const router = require('./router')
 const layouts = require("express-ejs-layouts")
 const port = 3000, express = require("express"), app = express();
 const path = require("path")
+const errorController = require("./controllers/errorController")
 
 app.set("view engine", "ejs")
 app.use(layouts)
@@ -9,6 +10,9 @@ app.use(layouts)
 app.use(express.json());
 app.use('/', router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
 
 app.listen(port, () => {
  console.log(`The Express.js server has started and is listening on port number: ${port}`);
