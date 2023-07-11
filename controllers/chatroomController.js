63cf31a0407fbb62c78c2c3d8a9879e4baa69717
sonkeks <Soenke.Schaarschmidt@Student.HTW-Berlin.de>
2023-07-12 01:29:10 +0200

    catch chatroom errors & auth

--- controllers/chatroomController.js
+++ controllers/chatroomController.js
@@ -2,42 +2,52 @@ const Chatroom = require("../models/chatroom");
 
 module.exports = {
     checkForExistingChatroom: (req, res, next) => {
-        let userId = req.user._id;
-        let requestedUserId = req.body.requestedUserId;
-        Chatroom.find({users: {$all: [userId, requestedUserId]}}).exec()
-            .then(chatrooms => {
-                if(chatrooms.length === 0) {
-                    next();
-                } else {
-                    //redirect to chat of both of these users
-                    let chatroom = chatrooms[0];
-                    res.redirect(`/chats/${chatroom._id}`);
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                res.redirect("/chats");
-            })
+        if(req.isAuthenticated()) {
+            let userId = req.user._id;
+            let requestedUserId = req.body.requestedUserId;
+            Chatroom.find({users: {$all: [userId, requestedUserId]}}).exec()
+                .then(chatrooms => {
+                    if (chatrooms.length === 0) {
+                        next();
+                    } else {
+                        //redirect to chat of both of these users
+                        let chatroom = chatrooms[0];
+                        res.redirect(`/chats/${chatroom._id}`);
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                    res.redirect("/chats");
+                })
+        } else {
+            req.flash("error", `You are not logged in`);
+            res.redirect("/login");
+        }
     },
     create: (req, res, next) => {
-        let chatroomParams = {
-            messages: [],
-            users: [req.user._id, req.body.requestedUserId],
-        };
-        Chatroom.create(chatroomParams)
-            .then(chatroom => {
-                console.log("Created new Chatroom: " + chatroom._id);
-                res.locals.redirect = `/chats/${chatroom._id}`;
-                next();
-            })
-            .catch(error => {
-                res.locals.redirect = `/`;
-                console.log("Error creating new Chatroom: " + error.message);
-                next();
-            })
+        if(req.isAuthenticated()) {
+            let chatroomParams = {
+                messages: [],
+                users: [req.user._id, req.body.requestedUserId],
+            };
+            Chatroom.create(chatroomParams)
+                .then(chatroom => {
+                    console.log("Created new Chatroom: " + chatroom._id);
+                    res.locals.redirect = `/chats/${chatroom._id}`;
+                    next();
+                })
+                .catch(error => {
+                    res.locals.redirect = `/`;
+                    console.log("Error creating new Chatroom: " + error.message);
+                    next();
+                })
+        } else {
+            req.flash("error", `You are not logged in`);
+            res.redirect("/login");
+        }
     },
     getAllChatrooms: (req, res, next) => {
-        if(req.isAuthenticated) {
+        if(req.isAuthenticated()) {
             let userId = req.user._id;
             Chatroom.find({users: {$in: [userId]}}).populate("users").exec()
                 .then(chatrooms => {
@@ -59,14 +69,22 @@ module.exports = {
     getChatroom: (req, res, next) => {
         if (req.isAuthenticated()) {
             let chatroomId = req.params.chatId;
+            let userId = req.user._id;
             Chatroom.findById(chatroomId).populate("users messages").exec()
                 .then(chatroom => {
-                    res.locals.chatroom = chatroom;
-                    next();
+                    let userIds = chatroom.users.map(user => {return user._id.toString()});
+                    if(userIds.includes(userId.toString())){
+                        res.locals.chatroom = chatroom;
+                        next();
+                    } else {
+                        req.flash("error", `You are not part of this chat`);
+                        res.redirect("/chats");
+                    }
                 })
                 .catch(error => {
                     console.log("Could not find chatroom: " + error);
-                    res.redirect("/");
+                    req.flash("error", `Sorry, we could not find the requested Chatroom`);
+                    res.redirect("/chats");
                 })
         } else {
             req.flash("error", `You are not logged in`);
